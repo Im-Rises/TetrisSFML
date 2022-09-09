3ce320b9d582d69be4e7d4acdf7af2728b3c130b
Im-Rises <quentin-morel88@hotmail.com>
2022-09-09 11:55:28 -0400

    Added all files

---
+++ TetrisSFML/TetrisSFML.cpp
@@ -0,0 +1,12 @@
+﻿// TetrisSFML.cpp : Defines the entry point for the application.
+//
+
+#include "TetrisSFML.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
