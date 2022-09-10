6b3b211b2feec70341c87b871d4bcd38a7ec526a
Im-Rises <quentin-morel88@hotmail.com>
2022-09-10 17:09:01 -0400

    Updated code behaviour

--- TetrisSFML/Tetris.cpp
+++ TetrisSFML/Tetris.cpp
@@ -3,106 +3,121 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <chrono>
+#include <iostream>
+#include <random>
+#include <cmath>
 
-
-#include "Tetromino.h"
 #include "tetrominos/Tetromino_o.h"
 
-Tetris::Tetris() {
+Tetris::Tetris() : window(sf::VideoMode(CELL_SIZE * COLUMNS * SCREEN_SIZE, CELL_SIZE * ROWS * SCREEN_SIZE),
+                          PROJECT_NAME),
+                   cell(sf::Vector2f(CELL_SIZE - 2, CELL_SIZE - 2)) {
+    window.setView(sf::View(sf::FloatRect(0, 0, CELL_SIZE * COLUMNS, CELL_SIZE * ROWS)));
+    for (auto &i: matrix) {
+        for (auto &j: i) {
+            j.state = false;
+            j.color = BACKGROUND_COLOR;
+        }
+    }
 }
 
 void Tetris::start() {
-    sf::Color backgroundColor(rgbBgColor[0], rgbBgColor[1], rgbBgColor[2]);
-    sf::RenderWindow window(sf::VideoMode(CELL_SIZE * COLUMNS * SCREEN_SIZE, CELL_SIZE * ROWS * SCREEN_SIZE),
-                            PROJECT_NAME);
-    window.setView(sf::View(sf::FloatRect(0, 0, CELL_SIZE * COLUMNS, CELL_SIZE * ROWS)));
-
-    sf::RectangleShape cell(sf::Vector2f(CELL_SIZE - 2, CELL_SIZE - 2));
-    std::vector<std::vector<bool>> matrix(COLUMNS, std::vector<bool>(ROWS, false));
-    std::vector<std::vector<sf::Color>> matrixColor(COLUMNS, std::vector<sf::Color>(ROWS, backgroundColor));
-
     auto previousTime = std::chrono::steady_clock::now();
-    unsigned int lagInput = 0;
-    Tetromino_o tetromino_o;
+
     while (window.isOpen()) {
         auto deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(
                 std::chrono::steady_clock::now() - previousTime).count();
+        handleEvents();
+        updateGame();
+        refreshScreen();
+    }
+}
 
-        // Handle tetrominos
-        bool moveDown = true;
-        for (auto &tile: tetromino_o.getTiles()) {
-            auto x = tile.x;
-            auto y = tile.y;
-            if (y == ROWS - 1 || matrix[x][y + 1]) {
-                moveDown = false;
+void Tetris::handleEvents() {
+    while (window.pollEvent(event)) {
+        switch (event.type) {
+            case sf::Event::Closed: {
+                window.close();
                 break;
             }
-        }
-        if (moveDown) {
-            tetromino_o.moveDown(ROWS);
-        } else {
-            for (auto &tile: tetromino_o.getTiles()) {
-                auto x = tile.x;
-                auto y = tile.y;
-                matrix[x][y] = 1;
-                matrixColor[x][y] = tetromino_o.getColor();
+            case sf::Event::MouseButtonPressed: {
+                auto mousePositionX = sf::Mouse::getPosition(window).x / CELL_SIZE / SCREEN_SIZE;
+                auto mousePositionY = sf::Mouse::getPosition(window).y / CELL_SIZE / SCREEN_SIZE;
+                matrix[mousePositionX][mousePositionY].state = 1 - matrix[mousePositionX][mousePositionY].state;
+                break;
             }
-            tetromino_o = Tetromino_o();
-        }
-
-        // Display
-        for (auto x = 0; x < COLUMNS; x++) {
-            for (auto y = 0; y < ROWS; y++) {
-                if (matrix[x][y] == 0) {
-                    cell.setFillColor(backgroundColor);
-                } else {
-                    cell.setFillColor(matrixColor[x][y]);
+            case sf::Event::KeyPressed: {
+                switch (event.key.code) {
+                    case sf::Keyboard::Escape:
+                        window.close();
+                        break;
+//                    case sf::Keyboard::Up:
+//                        fallingTetromino.rotate();
+//                        break;
+//                    case sf::Keyboard::Down:
+//                        fallingTetromino.hardMoveDown(ROWS);
+//                        break;
+//                    case sf::Keyboard::Left:
+//                        fallingTetromino.moveLeft(COLUMNS);
+//                        break;
+//                    case sf::Keyboard::Right:
+//                        fallingTetromino.moveRight(COLUMNS);
+//                        break;
+                    default:
+                        break;
                 }
-                cell.setPosition(CELL_SIZE * x + 1, CELL_SIZE * y + 1);
-                window.draw(cell);
+                break;
             }
-        }
-
-        for (auto &tile: tetromino_o.tiles) {
-            cell.setPosition(CELL_SIZE * tile.x + 1, CELL_SIZE * tile.y + 1);
-            cell.setFillColor(tetromino_o.color);
-            window.draw(cell);
-        }
-
-        window.display();
-
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            switch (event.type) {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                case sf::Event::KeyPressed:
-                    switch (event.key.code) {
-                        case sf::Keyboard::Escape:
-                            window.close();
-                            break;
-                        case sf::Keyboard::Up:
-                            //Rotate
-                            break;
-                        case sf::Keyboard::Down:
-                            tetromino_o.hardMoveDown(ROWS);
-                            break;
-                        case sf::Keyboard::Left:
-                            tetromino_o.moveLeft(COLUMNS);
-                            break;
-                        case sf::Keyboard::Right:
-                            tetromino_o.moveRight(COLUMNS);
-                            break;
-
-                    }
-                    break;
-                case sf::Event::MouseButtonPressed:
-                    auto mousePositionX = sf::Mouse::getPosition(window).x / CELL_SIZE / SCREEN_SIZE;
-                    auto mousePositionY = sf::Mouse::getPosition(window).y / CELL_SIZE / SCREEN_SIZE;
-                    matrix[mousePositionX][mousePositionY] = 1 - matrix[mousePositionX][mousePositionY];
-                    break;
+            default: {
+                break;
             }
         }
     }
 }
+
+void Tetris::updateGame() {
+    Tetromino_o tetromino_o;
+    bool moveDown = true;
+    for (auto &tile: tetromino_o.getTiles()) {
+        auto x = (int) tile.x;
+        auto y = (int) tile.y;
+        if (y == ROWS - 1 || matrix[x][y + 1].state) {
+            moveDown = false;
+            break;
+        }
+    }
+    if (moveDown) {
+        tetromino_o.moveDown(ROWS);
+    } else {
+        for (auto &tile: tetromino_o.getTiles()) {
+            matrix[(int) tile.x][(int) tile.y] = {true, tetromino_o.getColor()};
+        }
+        tetromino_o = Tetromino_o();
+    }
+}
+
+
+void Tetris::refreshScreen() {
+    // Background display
+    for (auto x = 0; x < COLUMNS; x++) {
+        for (auto y = 0; y < ROWS; y++) {
+            if (matrix[x][y].state) {
+                cell.setFillColor(BACKGROUND_COLOR);
+            } else {
+                cell.setFillColor(matrix[x][y].color);
+            }
+            cell.setPosition(CELL_SIZE * x + 1, CELL_SIZE * y + 1);
+            window.draw(cell);
+        }
+    }
+
+    // Falling tetromino display
+    for (auto &tile: fallingTetromino.getTiles()) {
+        cell.setPosition(CELL_SIZE * tile.x + 1, CELL_SIZE * tile.y + 1);
+        cell.setFillColor(fallingTetromino.color);
+        window.draw(cell);
+    }
+
+    window.display();
+}
+
--- TetrisSFML/Tetris.h
+++ TetrisSFML/Tetris.h
@@ -1,7 +1,11 @@
 #ifndef DEF_TETRIS
 #define DEF_TETRIS
 
+#include <SFML/Graphics.hpp>
+#include "tetrominos/Tetromino_o.h"
+
 #define PROJECT_NAME "Tetris"
+#define BACKGROUND_COLOR sf::Color(0, 100, 100)
 
 const unsigned char CELL_SIZE = 8;
 const unsigned char COLUMNS = 10;
@@ -10,15 +14,30 @@ const unsigned char SCREEN_SIZE = 4;
 
 const unsigned short FRAME_DURATION = 16;
 
-const unsigned char rgbBgColor[3] = {0, 100, 100};
-
 class Tetris {
 private:
+    sf::RenderWindow window;
+    sf::RectangleShape cell;
+    sf::Event event;
+
+    Tetromino_o fallingTetromino;
+
+    struct {
+        bool state = false;
+        sf::Color color = BACKGROUND_COLOR;
+    } matrix[COLUMNS][ROWS];
+
 public:
     Tetris();
     //	~Tetris();
 
     void start();
+
+    void handleEvents();
+
+    void updateGame();
+
+    void refreshScreen();
 };
 
 #endif // DEF_TETRIS
